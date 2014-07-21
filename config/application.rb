3cf1906f0f12d64e615909de53f9da9e8847c045
satpai1215 <pai.satyan@gmail.com>
2014-07-20 18:17:41 -0700

    app_name to flashcardz

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Flashcards
   class Application < Rails::Application
 
-    APP_NAME = "Flashcards"
+    APP_NAME = "Flashcardz"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
